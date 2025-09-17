391d18a052ff9ee2ebf34bd1563c1287c6c23abd
Buzzy35 <imbuzzy35@gmail.com>
2025-09-17 23:17:40 +0200

    implements end of game

--- js/script.js
+++ js/script.js
@@ -1,23 +1,28 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const startButton = document.getElementById('start');
+const scoreLabel = document.getElementById('score');
+
+const keys = {
+    ArrowLeft: false, ArrowRight: false
+};
 
 const ball = {
     x: canvas.width / 2,
     y: canvas.height / 2,
     radius: 20,
     color: '#000000',
-    speed: 1,
+    speed: 5,
     direction: Math.random() % Math.PI * 2,
 }
 
 const paddle = {
     x: (canvas.width / 2) - 50,
-    y: canvas.height - 20,
+    y: canvas.height - 50,
     width: 100,
     height: 10,
     color: '#1a57ca',
-    speed: 1,
+    speed: 5,
 }
 
 function drawBall() {
@@ -28,8 +33,24 @@ function drawBall() {
 }
 
 function updateBall() {
-    ball.x = ball.x + (10 * ball.speed * Math.cos(ball.direction));
-    ball.y = ball.y + (10 * ball.speed * Math.sin(ball.direction));
+    ball.x = ball.x + (ball.speed * Math.cos(ball.direction));
+    ball.y = ball.y + (ball.speed * Math.sin(ball.direction));
+
+    if ((ball.y + ball.radius >= paddle.y) && ball.x >= paddle.x && ball.x <= paddle.x + paddle.width) {
+        ball.direction = (Math.PI - ball.direction) % Math.PI * 2;
+        ball.y = paddle.y - ball.radius - 1;
+    } else if (ball.x - ball.radius <= 0) {
+        ball.direction = (Math.PI - ball.direction) % Math.PI * 2;
+        ball.x = ball.radius + 1;
+    } else if (ball.x + ball.radius >= canvas.width) {
+        ball.direction = (Math.PI - ball.direction) % Math.PI * 2;
+        ball.x = canvas.width - ball.radius - 1;
+    } else if (ball.y - ball.radius <= 0) {
+        ball.direction = (ball.direction + 2 * Math.PI) % Math.PI * 2;
+        ball.y = ball.radius + 1;
+    } else if (ball.y + ball.radius >= canvas.height) {
+        game = false;
+    }
     drawBall()
 }
 
@@ -40,40 +61,64 @@ function drawPaddle() {
     ctx.fill();
 }
 
-function updatePaddle(direction) {
-    paddle.x += direction * 10;
+function updatePaddle() {
+    if (keys.ArrowLeft) {
+        paddle.x -= paddle.speed;
+    }
+    if (keys.ArrowRight) {
+        paddle.x += paddle.speed;
+    }
     if (paddle.x + paddle.width > canvas.width) {
         paddle.x = canvas.width - paddle.width;
     }
     if (paddle.x < 0) {
         paddle.x = 0;
     }
-    drawPaddle()
+    drawPaddle();
 }
 
 function update() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     updateBall();
-    drawPaddle();
-}
-
-function game() {
-    update()
-    timer = requestAnimationFrame(game);
-}
-
-startButton.addEventListener('click', game);
-
-document.addEventListener('keydown', evt => {
-    switch (evt.key) {
-        case 'ArrowRight':
-            updatePaddle(1);
-            break;
-        case 'ArrowLeft':
-            updatePaddle(-1);
-            break;
+    updatePaddle();
+    scoreLabel.innerHTML = (Date.now() - startTime) / 1000;
+    if (game) {
+        requestAnimationFrame(update);
+    } else {
+        endGame()
     }
-})
+}
+
+function startGame() {
+    ball.x = canvas.width / 2;
+    ball.y = canvas.height / 2;
+    ball.direction = Math.random() % Math.PI * 2;
+    paddle.x = (canvas.width / 2) - 50;
+
+    startTime = Date.now();
+    game = true;
+    update();
+}
+
+function endGame() {
+    startButton.textContent = "Rejouer";
+    console.log('endGame');
+}
+
+startButton.addEventListener('click', startGame);
+
+
+document.addEventListener('keydown', e => {
+    if (e.key === 'ArrowLeft' || e.key === 'ArrowRight') {
+        keys[e.key] = true;
+    }
+});
+
+document.addEventListener('keyup', e => {
+    if (e.key === 'ArrowLeft' || e.key === 'ArrowRight') {
+        keys[e.key] = false;
+    }
+});
 
 
 drawBall()
